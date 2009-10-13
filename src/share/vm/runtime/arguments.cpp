f19b1c947e8fa52b9e1fb2e94455f7a4565293ba
never <devnull@localhost>
2009-10-13 12:04:21 -0700

    6890308: integrate zero assembler hotspot changes Reviewed-by: never Contributed-by: gbenson@redhat.com

--- src/share/vm/runtime/arguments.cpp
+++ src/share/vm/runtime/arguments.cpp
@@ -1236,6 +1236,7 @@ void Arguments::set_ergonomics_flags() {
     }
   }
 
+#ifndef ZERO
 #ifdef _LP64
   // Check that UseCompressedOops can be set with the max heap size allocated
   // by ergonomics.
@@ -1263,6 +1264,7 @@ void Arguments::set_ergonomics_flags() {
   // Also checks that certain machines are slower with compressed oops
   // in vm_version initialization code.
 #endif // _LP64
+#endif // !ZERO
 }
 
 void Arguments::set_parallel_gc_flags() {
