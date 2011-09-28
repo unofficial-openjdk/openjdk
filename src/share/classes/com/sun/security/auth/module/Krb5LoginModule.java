6c06f205fa9afd57f1c68a07206bca33afef4711
weijun <devnull@localhost>
2011-09-28 09:40:40 +0800

    7089889: Krb5LoginModule.login() throws an exception if used without a keytab Reviewed-by: xuelei, valeriep

--- src/share/classes/com/sun/security/auth/module/Krb5LoginModule.java
+++ src/share/classes/com/sun/security/auth/module/Krb5LoginModule.java
@@ -721,7 +721,7 @@ public class Krb5LoginModule implements LoginModule {
                         cred = builder.action().getCreds();
                     }
                     if (storeKey) {
-                        encKeys = builder.getKeys();
+                        encKeys = builder.getKeys(isInitiator);
                         // When encKeys is empty, the login actually fails.
                         // For compatibility, exception is thrown in commit().
                     }
