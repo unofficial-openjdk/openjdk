cb8850fa21a64bb49d33e7b503cdd044a1a9d29d
michaelm <devnull@localhost>
2012-01-30 16:17:53 +0000

    7139770: MacOS JCK failures in DatagramSocket and MulticastSocket Reviewed-by: chegar

--- src/share/classes/java/net/MulticastSocket.java
+++ src/share/classes/java/net/MulticastSocket.java
@@ -82,6 +82,13 @@ import java.util.Enumeration;
  */
 public
 class MulticastSocket extends DatagramSocket {
+
+    /**
+     * Used on some platforms to record if an outgoing interface
+     * has been set for this socket.
+     */
+    private boolean interfaceSet;
+
     /**
      * Create a multicast socket.
      *
@@ -163,15 +170,6 @@ class MulticastSocket extends DatagramSocket {
 
         if (bindaddr != null) {
             bind(bindaddr);
-        }
-        /**
-         * required for some platforms where it's not possible to join
-         * a group without setting the interface first.
-         */
-        NetworkInterface defaultInterface = NetworkInterface.getDefault();
-
-        if (defaultInterface != null) {
-            setNetworkInterface(defaultInterface);
         }
     }
 
@@ -306,6 +304,16 @@ class MulticastSocket extends DatagramSocket {
 
         if (!mcastaddr.isMulticastAddress()) {
             throw new SocketException("Not a multicast address");
+        }
+
+        /**
+         * required for some platforms where it's not possible to join
+         * a group without setting the interface first.
+         */
+        NetworkInterface defaultInterface = NetworkInterface.getDefault();
+
+        if (!interfaceSet && defaultInterface != null) {
+            setNetworkInterface(defaultInterface);
         }
 
         getImpl().join(mcastaddr);
@@ -458,6 +466,7 @@ class MulticastSocket extends DatagramSocket {
         synchronized (infLock) {
             getImpl().setOption(SocketOptions.IP_MULTICAST_IF, inf);
             infAddress = inf;
+            interfaceSet = true;
         }
     }
 
@@ -540,6 +549,7 @@ class MulticastSocket extends DatagramSocket {
         synchronized (infLock) {
             getImpl().setOption(SocketOptions.IP_MULTICAST_IF2, netIf);
             infAddress = null;
+            interfaceSet = true;
         }
     }
 
