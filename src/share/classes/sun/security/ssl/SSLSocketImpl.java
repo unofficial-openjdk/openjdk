4d2e37ed199231e9033d4581c9f5784180bb42e9
mbankal <devnull@localhost>
2012-07-17 10:51:40 -0700

    7184700: Backout changes with wrong id for 7157903 Reviewed-by: asaha

--- src/share/classes/sun/security/ssl/SSLSocketImpl.java
+++ src/share/classes/sun/security/ssl/SSLSocketImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2011, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -363,12 +363,6 @@ final public class SSLSocketImpl extends BaseSSLSocketImpl {
      */
     private boolean isFirstAppOutputRecord = true;
 
-    /*
-     * If AppOutputStream needs to delay writes of small packets, we
-     * will use this to store the data until we actually do the write.
-     */
-    private ByteArrayOutputStream heldRecordBuffer = null;
-
     //
     // CONSTRUCTORS AND INITIALIZATION CODE
     //
@@ -634,24 +628,13 @@ final public class SSLSocketImpl extends BaseSSLSocketImpl {
     //
 
     /*
-     * AppOutputStream calls may need to buffer multiple outbound
-     * application packets.
-     *
-     * All other writeRecord() calls will not buffer, so do not hold
-     * these records.
-     */
-    void writeRecord(OutputRecord r) throws IOException {
-        writeRecord(r, false);
-    }
-
-    /*
      * Record Output. Application data can't be sent until the first
      * handshake establishes a session.
      *
      * NOTE:  we let empty records be written as a hook to force some
      * TCP-level activity, notably handshaking, to occur.
      */
-    void writeRecord(OutputRecord r, boolean holdRecord) throws IOException {
+    void writeRecord(OutputRecord r) throws IOException {
         /*
          * The loop is in case of HANDSHAKE --> ERROR transitions, etc
          */
@@ -722,7 +705,7 @@ final public class SSLSocketImpl extends BaseSSLSocketImpl {
                 try {
                     if (writeLock.tryLock(getSoLinger(), TimeUnit.SECONDS)) {
                         try {
-                            writeRecordInternal(r, holdRecord); 
+                            writeRecordInternal(r);
                         } finally {
                             writeLock.unlock();
                         }
@@ -770,7 +753,7 @@ final public class SSLSocketImpl extends BaseSSLSocketImpl {
             } else {
                 writeLock.lock();
                 try {
-                    writeRecordInternal(r, holdRecord);
+                    writeRecordInternal(r);
                 } finally {
                     writeLock.unlock();
                 }
@@ -778,27 +761,11 @@ final public class SSLSocketImpl extends BaseSSLSocketImpl {
         }
     }
 
-    private void writeRecordInternal(OutputRecord r, 
-            boolean holdRecord) throws IOException { 
+    private void writeRecordInternal(OutputRecord r) throws IOException {
         // r.compress(c);
         r.addMAC(writeMAC);
         r.encrypt(writeCipher);
-        if (holdRecord) { 
-            // If we were requested to delay the record due to possibility 
-            // of Nagle's being active when finally got to writing, and 
-            // it's actually not, we don't really need to delay it. 
-            if (getTcpNoDelay()) { 
-                holdRecord = false; 
-            } else { 
-                // We need to hold the record, so let's provide 
-                // a per-socket place to do it. 
-                if (heldRecordBuffer == null) { 
-                    // Likely only need 37 bytes. 
-                    heldRecordBuffer = new ByteArrayOutputStream(40); 
-                } 
-            } 
-        } 
-        r.write(sockOutput, holdRecord, heldRecordBuffer);
+        r.write(sockOutput);
 
         // turn off the flag of the first application record
         if (isFirstAppOutputRecord &&
