58ba75bbb128ba3f8d8d6d594aa4b789efc90097
bae <devnull@localhost>
2012-08-29 11:52:44 +0400

    7150594: VM chash in JCK api/java_awt/Image/ConvolveOp/ tests for 64 bit jdk8 on linux. Reviewed-by: jgodinez, prr

--- src/share/native/sun/awt/medialib/mlib_types.h
+++ src/share/native/sun/awt/medialib/mlib_types.h
@@ -59,8 +59,16 @@ typedef double             mlib_d64;
 
 #if defined(__SUNPRO_C) || defined(__SUNPRO_CC) || defined(__GNUC__)
 
+#if defined(MACOSX)
 #include <stddef.h>                     /* for ptrdiff_t */
 #include <stdint.h>                     /* for uintptr_t */
+#elif defined(__linux__)
+#include <stdint.h>                     /* for uintptr_t */
+#include <malloc.h>                     /* for ptrdiff_t */
+#else
+#include <link.h>                       /* for uintptr_t */
+#include <stddef.h>                     /* for ptrdiff_t */
+#endif  /* __linux__ */
 
 #if defined(MLIB_OS64BIT) || (defined(MACOSX) && defined(_LP64))
 
