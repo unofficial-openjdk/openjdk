a96af351233d2fb203816a6780f032f8c2d4dba8
roland <devnull@localhost>
2012-09-20 16:49:17 +0200

    7023898: Intrinsify AtomicLongFieldUpdater.getAndIncrement() Summary: use shorter instruction sequences for atomic add and atomic exchange when possible. Reviewed-by: kvn, jrose

--- src/share/vm/opto/connode.cpp
+++ src/share/vm/opto/connode.cpp
@@ -479,7 +479,9 @@ static bool can_cause_alias(Node *n, PhaseTransform *phase) {
         opc == Op_CheckCastPP ||
         opc == Op_StorePConditional ||
         opc == Op_CompareAndSwapP ||
-        opc == Op_CompareAndSwapN;
+        opc == Op_CompareAndSwapN ||
+        opc == Op_GetAndSetP ||
+        opc == Op_GetAndSetN;
   }
   return possible_alias;
 }
