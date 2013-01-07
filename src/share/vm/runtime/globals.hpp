900846210da30d57575464eeb1ecbb60932e0954
dcubed <devnull@localhost>
2013-01-07 12:56:30 -0800

    8001307: Modify ACC_SUPER behavior Summary: Disallow non-virtual calls even when ACC_SUPER is absent. Reviewed-by: dcubed Contributed-by: paul.nauman@oracle.com

--- src/share/vm/runtime/globals.hpp
+++ src/share/vm/runtime/globals.hpp
@@ -3700,7 +3700,10 @@ class CommandLineFlags {
   product(bool, UseVMInterruptibleIO, false,                                \
           "(Unstable, Solaris-specific) Thread interrupt before or with "   \
           "EINTR for I/O operations results in OS_INTRPT. The default value"\
-          " of this flag is true for JDK 6 and earliers")
+          " of this flag is true for JDK 6 and earliers")                   \
+                                                                            \
+  product(bool, AllowNonVirtualCalls, false,                                \
+          "Obey the ACC_SUPER flag and allow invokenonvirtual calls")
 
 /*
  *  Macros for factoring of globals
