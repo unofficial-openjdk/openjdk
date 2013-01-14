aef83b7ac151a3b73ad8a02fb29d94ef38dc02b9
ptbrunet <devnull@localhost>
2013-01-14 23:22:03 +0000

    7177111: Jcomponent.AccessibleJComponent.AddPropertyListeners adds exponential listeners Summary: Remove field javax.swing.JComponent.AccessibleJComponent.accessibleContainerHandler Reviewed-by: art

--- src/share/classes/javax/swing/JComponent.java
+++ src/share/classes/javax/swing/JComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -3686,7 +3686,6 @@ public abstract class JComponent extends Container implements Serializable,
             super();
         }
 
-        protected ContainerListener accessibleContainerHandler = null;
         protected FocusListener accessibleFocusHandler = null;
 
         /**
