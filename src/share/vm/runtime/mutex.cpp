6559c93646787f5102a66622287f931103325587
rbackman <devnull@localhost>
2013-01-18 13:43:56 +0100

    8006563: Remove unused ProfileVM_lock Reviewed-by: dholmes, sla

--- src/share/vm/runtime/mutex.cpp
+++ src/share/vm/runtime/mutex.cpp
@@ -1312,7 +1312,7 @@ void Monitor::set_owner_implementation(Thread *new_owner) {
           this->rank() != Mutex::suspend_resume &&
           locks != NULL && locks->rank() <= this->rank() &&
           !SafepointSynchronize::is_at_safepoint() &&
-          this != Interrupt_lock && this != ProfileVM_lock &&
+          this != Interrupt_lock &&
           !(this == Safepoint_lock && contains(locks, Terminator_lock) &&
             SafepointSynchronize::is_synchronizing())) {
         new_owner->print_owned_locks();
