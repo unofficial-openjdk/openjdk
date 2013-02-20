659b448d38326a1295fc057720e87ee08d53eba3
poonam <devnull@localhost>
2013-02-19 16:03:07 -0800

    8006938: Change os::print_location to be more descriptive when a location is pointing into an object Reviewed-by: stefank, twisti, poonam Contributed-by: sergey.gabdurakhmanov@oracle.com

--- src/share/vm/runtime/os.cpp
+++ src/share/vm/runtime/os.cpp
@@ -890,7 +890,11 @@ void os::print_location(outputStream* st, intptr_t x, bool verbose) {
       print = true;
     }
     if (print) {
-      st->print_cr(INTPTR_FORMAT " is an oop", addr);
+      if (p == (HeapWord*) addr) {
+        st->print_cr(INTPTR_FORMAT " is an oop", addr);
+      } else {
+        st->print_cr(INTPTR_FORMAT " is pointing into object: " INTPTR_FORMAT, addr, p);
+      }
       oop(p)->print_on(st);
       if (p != (HeapWord*)x && oop(p)->is_constMethod() &&
           constMethodOop(p)->contains(addr)) {
