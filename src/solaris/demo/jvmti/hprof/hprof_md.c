f922825e2719eb556aad15560bd0e7621c358635
sla <devnull@localhost>
2013-03-05 19:25:35 +0100

    8009397: test/com/sun/jdi/PrivateTransportTest.sh: ERROR: transport library missing onLoad entry: private_dt_socket Reviewed-by: alanb

--- src/solaris/demo/jvmti/hprof/hprof_md.c
+++ src/solaris/demo/jvmti/hprof/hprof_md.c
@@ -380,6 +380,7 @@ static void dll_build_name(char* buffer, size_t buflen,
     // Loosely based on os_solaris.cpp
 
       char *pathname = (char *)pname;
+      *buffer = '\0';
       while (strlen(pathname) > 0) {
           char *p = strchr(pathname, ':');
           if (p == NULL) {
@@ -390,12 +391,16 @@ static void dll_build_name(char* buffer, size_t buflen,
               continue;
           }
           (void)snprintf(buffer, buflen, "%.*s/lib%s" JNI_LIB_SUFFIX,
-                         (p - pathname), pathname, fname);
+                         (int)(p - pathname), pathname, fname);
 
           if (access(buffer, F_OK) == 0) {
-            break;
+              break;
           }
-          pathname = p + 1;
+          if (*p == '\0') {
+              pathname = p;
+          } else {
+              pathname = p + 1;
+          }
           *buffer = '\0';
       }
 }
