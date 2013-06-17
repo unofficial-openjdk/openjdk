cd731cecd38c733ad13c0e10f9ac3becc6fd8dff
coffeys <devnull@localhost>
2013-06-17 19:01:08 +0200

    8000642: Better handling of objects for transportation Summary: Also reviewed by alexander.fomin Reviewed-by: alanb, mchung, skoivu, omajid

--- src/share/classes/com/sun/corba/se/impl/interceptors/RequestInfoImpl.java
+++ src/share/classes/com/sun/corba/se/impl/interceptors/RequestInfoImpl.java
@@ -658,7 +658,8 @@ public abstract class RequestInfoImpl
             // Convert the "core" service context to an
             // "IOP" ServiceContext by writing it to a
             // CDROutputStream and reading it back.
-            EncapsOutputStream out = new EncapsOutputStream(myORB);
+            EncapsOutputStream out =
+                sun.corba.OutputStreamFactory.newEncapsOutputStream(myORB);
 
             context.write( out, GIOPVersion.V1_2 );
             InputStream inputStream = out.create_input_stream();
@@ -694,8 +695,8 @@ public abstract class RequestInfoImpl
     {
         int id = 0 ;
         // Convert IOP.service_context to core.ServiceContext:
-        EncapsOutputStream outputStream = new EncapsOutputStream(
-            myORB );
+        EncapsOutputStream outputStream =
+           sun.corba.OutputStreamFactory.newEncapsOutputStream(myORB);
         InputStream inputStream = null;
         UnknownServiceContext coreServiceContext = null;
         ServiceContextHelper.write( outputStream, service_context );
