270a6a567e72284b36cc99eb096d0ce654d73b98
andrew <devnull@localhost>
2013-07-15 21:09:53 +0100

    8004261: Improve input validation Reviewed-by: art, mschoene, amenkov, omajid

--- src/share/classes/com/sun/media/sound/RealTimeSequencer.java
+++ src/share/classes/com/sun/media/sound/RealTimeSequencer.java
@@ -1019,7 +1019,7 @@ class RealTimeSequencer extends AbstractMidiDevice implements Sequencer, AutoCon
 
     class SequencerReceiver extends AbstractReceiver {
 
-        protected void implSend(MidiMessage message, long timeStamp) {
+        void implSend(MidiMessage message, long timeStamp) {
             if (recording) {
                 long tickPos = 0;
 
