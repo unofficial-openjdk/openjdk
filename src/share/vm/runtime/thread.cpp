553ba2b29d4ab25df1bfca7b5c6800afc6dc8184
allwin <devnull@localhost>
2013-07-17 10:52:31 +0200

    7162400: Intermittent java.io.IOException: Bad file number during HotSpotVirtualMachine.executeCommand Reviewed-by: dcubed, dholmes, sspitsyn, mgerdin, ctornqvi, dsamersoff

--- src/share/vm/runtime/thread.cpp
+++ src/share/vm/runtime/thread.cpp
@@ -3608,6 +3608,7 @@ jint Threads::create_vm(JavaVMInitArgs* args, bool* canTryAgain) {
 
   // Start Attach Listener if +StartAttachListener or it can't be started lazily
   if (!DisableAttachMechanism) {
+    AttachListener::vm_start();
     if (StartAttachListener || AttachListener::init_at_startup()) {
       AttachListener::init();
     }
