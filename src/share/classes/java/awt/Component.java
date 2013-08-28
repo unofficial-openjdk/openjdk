edd11ad2e69eb43e78c5e8917ecb511b355134f0
dmarkov <devnull@localhost>
2013-08-28 16:12:36 +0400

    8023474: First mousepress doesn't start editing in JTree Reviewed-by: alexp, anthony

--- src/share/classes/java/awt/Component.java
+++ src/share/classes/java/awt/Component.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1995, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1995, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -971,6 +971,10 @@ public abstract class Component implements ImageObserver, MenuContainer,
 
             public AccessControlContext getAccessControlContext(Component comp) {
                 return comp.getAccessControlContext();
+            }
+
+            public void revalidateSynchronously(Component comp) {
+                comp.revalidateSynchronously();
             }
         });
     }
@@ -2963,6 +2967,13 @@ public abstract class Component implements ImageObserver, MenuContainer,
      * @since 1.7
      */
     public void revalidate() {
+        revalidateSynchronously();
+    }
+
+    /**
+     * Revalidates the component synchronously.
+     */
+    final void revalidateSynchronously() {
         synchronized (getTreeLock()) {
             invalidate();
 
