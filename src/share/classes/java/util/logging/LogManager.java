2e0ec55b60ef282681b34383df3d1e0a0e18b47e
dfuchs <devnull@localhost>
2013-10-07 12:09:22 +0200

    8024867: Enhance logging start up Reviewed-by: mchung, hawtin

--- src/share/classes/java/util/logging/LogManager.java
+++ src/share/classes/java/util/logging/LogManager.java
@@ -257,6 +257,11 @@ public class LogManager {
      * retrieved by calling Logmanager.getLogManager.
      */
     protected LogManager() {
+        this(checkSubclassPermissions());
+    }
+
+    private LogManager(Void checked) {
+
         // Add a shutdown hook to close the global handlers.
         try {
             Runtime.getRuntime().addShutdownHook(new Cleaner());
@@ -264,6 +269,19 @@ public class LogManager {
             // If the VM is already shutting down,
             // We do not need to register shutdownHook.
         }
+    }
+
+    private static Void checkSubclassPermissions() {
+        final SecurityManager sm = System.getSecurityManager();
+        if (sm != null) {
+            // These permission will be checked in the LogManager constructor,
+            // in order to register the Cleaner() thread as a shutdown hook.
+            // Check them here to avoid the penalty of constructing the object
+            // etc...
+            sm.checkPermission(new RuntimePermission("shutdownHooks"));
+            sm.checkPermission(new RuntimePermission("setContextClassLoader"));
+        }
+        return null;
     }
 
     /**
