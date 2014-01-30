5ebd106760d9b2b74ad64f6c699c5d747d17890d
miroslawzn <devnull@localhost>
2014-01-30 13:28:59 -0800

    8011944: Sort fails with ArrayIndexOutOfBoundsException Reviewed-by: alanb

--- src/share/classes/java/util/TimSort.java
+++ src/share/classes/java/util/TimSort.java
@@ -158,7 +158,7 @@ class TimSort<T> {
          */
         int stackLen = (len <    120  ?  5 :
                         len <   1542  ? 10 :
-                        len < 119151  ? 19 : 40);
+                        len < 119151  ? 24 : 40);
         runBase = new int[stackLen];
         runLen = new int[stackLen];
     }
