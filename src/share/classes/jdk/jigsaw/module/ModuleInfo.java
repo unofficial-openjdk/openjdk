78c7cefbd6caf99e68402e9ad7e2bb78cd1fa46e
alanb <devnull@localhost>
2014-03-24 13:07:37 +0000

    Update module path support to read javac generated module-info.class files

--- src/share/classes/jdk/jigsaw/module/ModuleInfo.java
+++ src/share/classes/jdk/jigsaw/module/ModuleInfo.java
@@ -25,6 +25,7 @@
 
 package jdk.jigsaw.module;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collections;
@@ -32,155 +33,352 @@ import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Set;
 
 import jdk.jigsaw.module.ModuleDependence.Modifier;
 
 /**
- * Information about a module, as found in a {@code module-info.java} source
- * file or a {@code module-info.class} class file.
- *
- * For now this class treats the module-info.class as a properties file. This
- * is for testing purposes only and will be replaced with a class file reader
- * once javac is compiling module-info source files.
+ * Represents module information as read from a {@code module-info} class file
+ * and defines a method to combine this with the module contents to create a
+ * {@link Module}.
  */
 public final class ModuleInfo {
-    private final Properties props;
-    private ModuleInfo(Properties props) {
-        this.props = props;
+
+    // The name of the Module attribute
+    private static final String MODULE = "Module";
+
+    // access and requires flags
+    private static final int ACC_MODULE      = 0x8000;
+    private static final int ACC_PUBLIC      = 0x0020;
+
+    private final String name;
+    private final Set<ModuleDependence> moduleDependences = new HashSet<>();
+
+    // optional and created lazily
+    private Set<ServiceDependence> serviceDependences;
+    private Set<String> permits;
+    private Set<ModuleExport> exports;
+    private Map<String, Set<String>> services;
+
+    /**
+     * Returns the module name as found in the this_class item of
+     * the class file.
+     */
+    public String name() {
+        return name;
     }
 
     /**
      * Reads a {@code module-info.class} from the given input stream.
+     *
+     * @throws ClassFormatError if the class file is malformed
+     * @throws IOException if an I/O errors occurs
      */
     public static ModuleInfo read(InputStream in) throws IOException {
-        Properties props = new Properties();
-        props.load(in);
-        ModuleInfo mi = new ModuleInfo(props);
-        if (mi.name() == null)
-            throw new IOException("Module information incomplete, no module name");
-        return mi;
+        return new ModuleInfo(new DataInputStream(in));
     }
 
-    /**
-     * Returns the module name.
-     */
-    public String name() {
-        return props.getProperty("name");
-    }
+    private ModuleInfo(DataInputStream in) throws IOException {
+        int magic = in.readInt();
+        if (magic != 0xCAFEBABE)
+            throw new ClassFormatError("Bad magic number");
 
-    /**
-     * Returns the set of the module names that are required. The returned set does
-     * not include the names of the modules that are required with the
-     * {@code ACC_PUBLIC} flag set.
-     */
-    public Set<String> requires() {
-        return split("requires");
-    }
-
-    /**
-     * Returns the set of the module names that are permitted (maybe empty).
-     */
-    public Set<String> permits() {
-        return split("permits");
-    }
-
-    /**
-     *  Returns the set of the module names that are required with the
-     *  {@code ACC_PUBLIC} flag set.
-     */
-    public Set<String> requiresPublic() {
-        return split("requires-public");
-    }
-
-    /**
-     * Returns the set of package names that are exported. The returned set does
-     * the package names that are exported so that they can only be accessed by
-     * particular modules.
-     */
-    public Set<String> exports() {
-        return split("exports");
-    }
-
-    /**
-     * Returns a map of the service providers that the module provides.
-     * The map key is the type name of the service interface. The map key
-     * is the set of type names for the service implementations.
-     */
-    public Map<String, Set<String>> provides() {
-        Set<String> values = split("provides");
-        if (values.isEmpty())
-            return Collections.emptyMap();
-
-        Map<String, Set<String>> result = new HashMap<>();
-        for (String value: values) {
-            String[] s = value.split(" ");
-            if (s.length == 3 && s[1].equals("with")) {
-                String service = s[0];
-                String provider = s[2];
-                result.computeIfAbsent(service, k -> new HashSet<>()).add(provider);
-             }
+        int minor_version = in.readUnsignedShort();
+        int major_version = in.readUnsignedShort();
+        if (major_version < 53) {
+            // throw new ClassFormatError("Must be >= 53.0");
         }
-        return result;
+
+        ConstantPool cpool = new ConstantPool(in);
+
+        int access_flags = in.readUnsignedShort();
+        if (access_flags != ACC_MODULE) {
+            // throw new ClassFormatError("access_flags should be ACC_MODULE");
+        }
+
+        int this_class = in.readUnsignedShort();
+        String mn = cpool.getClassName(this_class);
+        int suffix = mn.indexOf("/module-info");
+        if (suffix < 1)
+            throw new ClassFormatError("this_class not of form name/module-info");
+        this.name = mn.substring(0, suffix);
+
+        int super_class = in.readUnsignedShort();
+        if (super_class > 0)
+            throw new ClassFormatError("bad #super_class");
+
+        int interfaces_count = in.readUnsignedShort();
+        if (interfaces_count > 0)
+            throw new ClassFormatError("Bad #interfaces");
+
+        int fields_count = in.readUnsignedShort();
+        if (fields_count > 0)
+            throw new ClassFormatError("Bad #fields");
+
+        int methods_count = in.readUnsignedShort();
+        if (methods_count > 0)
+            throw new ClassFormatError("Bad #fields");
+
+        int attributes_count = in.readUnsignedShort();
+        boolean found = false;
+        for (int i = 0; i < attributes_count ; i++) {
+            int name_index = in.readUnsignedShort();
+            String name = cpool.getUtf8(name_index);
+            int length = in.readInt();
+            switch (name) {
+                case MODULE :
+                    if (found)
+                        throw new ClassFormatError("More than one Module attribute");
+                    readModuleAttribute(in, cpool);
+                    found = true;
+                    break;
+                default:
+                    // Should check that it's one of: Synthetic, SourceFile,
+                    // SourceDebugExtension, Deprecated.
+                    in.skip(length);
+            }
+        }
+        if (!found)
+            throw new ClassFormatError("Missing Module attribute");
     }
 
     /**
-     * Creates a module definition from the module information and the given
-     * content.
+     * Reads the Module attribute.
+     */
+    private void readModuleAttribute(DataInputStream in, ConstantPool cpool)
+        throws IOException
+    {
+        int requires_count = in.readUnsignedShort();
+        if (requires_count == 0 && !name.equals("java.base"))
+            throw new ClassFormatError("The requires table must have at least one entry");
+        for (int i=0; i<requires_count; i++) {
+            int index = in.readUnsignedShort();
+            int flags = in.readUnsignedShort();
+            String dn = cpool.getUtf8(index);
+            Set<Modifier> mods = ((flags & ACC_PUBLIC) != 0) ?
+                    EnumSet.of(Modifier.PUBLIC) : Collections.emptySet();
+            moduleDependences.add(new ModuleDependence(mods, ModuleIdQuery.parse(dn)));
+        }
+
+        int permits_count = in.readUnsignedShort();
+        if (permits_count > 0) {
+            permits = new HashSet<>();
+            for (int i=0; i<permits_count; i++) {
+                int index = in.readUnsignedShort();
+                permits.add(cpool.getUtf8(index));
+            }
+        }
+
+        int exports_count = in.readUnsignedShort();
+        if (exports_count > 0) {
+            exports = new HashSet<>();
+            for (int i=0; i<exports_count; i++) {
+                int index = in.readUnsignedShort();
+                String pkg = cpool.getUtf8(index).replace('/', '.');
+                int exports_to_count = in.readUnsignedShort();
+                if (exports_to_count > 0) {
+                    for (int j=0; j<exports_to_count; j++) {
+                        int exports_to_index = in.readUnsignedShort();
+                        String permit = cpool.getUtf8(exports_to_index);
+                        exports.add(new ModuleExport(pkg, permit));
+                    }
+                } else {
+                    exports.add(new ModuleExport(pkg));
+                }
+            }
+        }
+
+        int uses_count = in.readUnsignedShort();
+        if (uses_count > 0) {
+            serviceDependences = new HashSet<>();
+            for (int i=0; i<uses_count; i++) {
+                int index = in.readUnsignedShort();
+                String sn = cpool.getClassName(index).replace('/', '.');
+                serviceDependences.add(new ServiceDependence(null, sn));
+            }
+        }
+
+        int provides_count = in.readUnsignedShort();
+        if (provides_count > 0) {
+            services = new HashMap<>();
+            for (int i=0; i<provides_count; i++) {
+                int index = in.readUnsignedShort();
+                int with_index = in.readUnsignedShort();
+                String sn = cpool.getClassName(index).replace('/', '.');
+                String cn = cpool.getClassName(with_index).replace('/', '.');
+                services.computeIfAbsent(sn, k -> new HashSet<>()).add(cn);
+            }
+        }
+    }
+
+    /**
+     * Creates a {@code Module} from the information in the module-info file
+     * and the given set of packages (the module content).
      */
     public Module makeModule(Iterable<String> packages) {
         Module.Builder builder = new Module.Builder();
-        builder.id(name());
+
+        // from module-info
+        builder.id(name);
+        moduleDependences.forEach(builder::requires);
+        if (serviceDependences != null)
+            serviceDependences.forEach(builder::requires);
+        if (permits != null)
+            permits.forEach(builder::permit);
+        if (exports != null)
+            exports.forEach(pkg -> builder.export(pkg));
+        if (services != null) {
+            for (Map.Entry<String, Set<String>> entry: services.entrySet()) {
+                String sn = entry.getKey();
+                entry.getValue().forEach(cn -> builder.service(sn, cn));
+            }
+        }
 
         // contents
         packages.forEach(pkg -> builder.include(pkg));
 
-        // unqualified exports
-        exports().forEach(pkg -> builder.export(pkg));
-
-        // requires
-        requires().forEach( dn ->
-            builder.requires(new ModuleDependence(null, ModuleIdQuery.parse(dn))) );
-
-        // requires public
-        Set<String> requiresPublic = requiresPublic();
-        if (!requiresPublic.isEmpty()) {
-            Set<Modifier> mods = EnumSet.of(Modifier.PUBLIC);
-            requiresPublic.forEach( dn ->
-                builder.requires(new ModuleDependence(mods, ModuleIdQuery.parse(dn))));
-        }
-
-        // add requires java.base if no requires
-        if (requires().isEmpty() && requiresPublic().isEmpty()) {
-            builder.requires(new ModuleDependence(null, ModuleIdQuery.parse("java.base")));
-        }
-
-        // permits
-        Set<String> permits = permits();
-        if (!permits.isEmpty()) {
-            permits.forEach(m -> builder.permit(m));
-        }
-
-        // services
-        for (Map.Entry<String, Set<String>> entry: provides().entrySet()) {
-            String s = entry.getKey();
-            entry.getValue().forEach(p -> builder.service(s, p));
-        }
-
         return builder.build();
     }
 
-    private Set<String> split(String prop) {
-        String values = props.getProperty(prop);
-        if (values == null) {
-            return Collections.emptySet();
-        } else {
-            Set<String> result = new HashSet<>();
-            for (String value: values.split(",")) {
-                result.add(value.trim());
+    /**
+     * The constant pool in a class file.
+     */
+    private static class ConstantPool {
+        static final int CONSTANT_Utf8 = 1;
+        static final int CONSTANT_Integer = 3;
+        static final int CONSTANT_Float = 4;
+        static final int CONSTANT_Long = 5;
+        static final int CONSTANT_Double = 6;
+        static final int CONSTANT_Class = 7;
+        static final int CONSTANT_String = 8;
+        static final int CONSTANT_Fieldref = 9;
+        static final int CONSTANT_Methodref = 10;
+        static final int CONSTANT_InterfaceMethodref = 11;
+        static final int CONSTANT_NameAndType = 12;
+        static final int CONSTANT_MethodHandle = 15;
+        static final int CONSTANT_MethodType = 16;
+        static final int CONSTANT_InvokeDynamic = 18;
+
+        private static class Entry {
+            protected Entry(int tag) {
+                this.tag = tag;
             }
-            return result;
+            final int tag;
+        }
+
+        private static class IndexEntry extends Entry {
+            IndexEntry(int tag, int index) {
+                super(tag);
+                this.index = index;
+            }
+            final int index;
+        }
+
+        private static class Index2Entry extends Entry {
+            Index2Entry(int tag, int index1, int index2) {
+                super(tag);
+                this.index1 = index1;
+                this.index2 = index2;
+            }
+            final int index1,  index2;
+        }
+
+        private static class ValueEntry extends Entry {
+            ValueEntry(int tag, Object value) {
+                super(tag);
+                this.value = value;
+            }
+            final Object value;
+        }
+
+        final Entry[] pool;
+
+        ConstantPool(DataInputStream in) throws IOException {
+            int count = in.readUnsignedShort();
+            pool = new Entry[count];
+
+            for (int i = 1; i < count; i++) {
+                int tag = in.readUnsignedByte();
+                switch (tag) {
+                    case CONSTANT_Class:
+                    case CONSTANT_String:
+                        int index = in.readUnsignedShort();
+                        pool[i] = new IndexEntry(tag, index);
+                        break;
+
+                    case CONSTANT_Double:
+                        double dvalue = in.readDouble();
+                        pool[i] = new ValueEntry(tag, dvalue);
+                        i++;
+                        break;
+
+                    case CONSTANT_Fieldref:
+                    case CONSTANT_InterfaceMethodref:
+                    case CONSTANT_Methodref:
+                    case CONSTANT_InvokeDynamic:
+                    case CONSTANT_NameAndType:
+                        int index1 = in.readUnsignedShort();
+                        int index2 = in.readUnsignedShort();
+                        pool[i] = new Index2Entry(tag, index1, index2);
+                        break;
+
+                    case CONSTANT_MethodHandle:
+                        int refKind = in.readUnsignedByte();
+                        index = in.readUnsignedShort();
+                        pool[i] = new Index2Entry(tag, refKind, index);
+                        break;
+
+                    case CONSTANT_MethodType:
+                        index = in.readUnsignedShort();
+                        pool[i] = new IndexEntry(tag, index);
+                        break;
+
+                    case CONSTANT_Float:
+                        float fvalue = in.readFloat();
+                        pool[i] = new ValueEntry(tag, fvalue);
+                        break;
+
+                    case CONSTANT_Integer:
+                        int ivalue = in.readInt();
+                        pool[i] = new ValueEntry(tag, ivalue);
+                        break;
+
+                    case CONSTANT_Long:
+                        long lvalue = in.readLong();
+                        pool[i] = new ValueEntry(tag, lvalue);
+                        i++;
+                        break;
+
+                    case CONSTANT_Utf8:
+                        String svalue = in.readUTF();
+                        pool[i] = new ValueEntry(tag, svalue);
+                        break;
+
+                    default:
+                        throw new ClassFormatError("Bad constant pool entry" + i);
+                }
+            }
+        }
+
+        String getClassName(int index) {
+            Entry e = pool[index];
+            assert e.tag == CONSTANT_Class;
+            return getUtf8(((IndexEntry) e).index);
+        }
+
+        String getUtf8(int index) {
+            Entry e = pool[index];
+            assert e.tag == CONSTANT_Utf8;
+            return (String) (((ValueEntry) e).value);
+        }
+
+        Object getValue(int index) {
+            Entry e = pool[index];
+            assert e.tag == CONSTANT_Double ||
+                    e.tag == CONSTANT_Float ||
+                    e.tag == CONSTANT_Integer ||
+                    e.tag == CONSTANT_Long;
+            return ((ValueEntry) e).value;
         }
     }
 }
