d42d9c076351c5ee15c0edf9749cc0a10b382bf9
igerasim <devnull@localhost>
2014-11-17 20:20:20 +0300

    8062807: Exporting RMI objects fails when run under restrictive SecurityManager Reviewed-by: smarks

--- src/share/classes/sun/rmi/transport/tcp/TCPTransport.java
+++ src/share/classes/sun/rmi/transport/tcp/TCPTransport.java
@@ -672,22 +672,22 @@ public class TCPTransport extends Transport {
         }
 
         public void run() {
-            AccessController.doPrivileged(new PrivilegedAction<Void>() {
+            Thread t = Thread.currentThread();
+            String name = t.getName();
+            try {
+                t.setName("RMI TCP Connection(" +
+                          connectionCount.incrementAndGet() +
+                          ")-" + remoteHost);
+                AccessController.doPrivileged(new PrivilegedAction<Void>() {
                     @Override
                     public Void run() {
-                        Thread t = Thread.currentThread();
-                        String name = t.getName();
-                        try {
-                            t.setName("RMI TCP Connection(" +
-                                      connectionCount.incrementAndGet() +
-                                      ")-" + remoteHost);
-                            run0();
-                        } finally {
-                            t.setName(name);
-                        }
+                        run0();
                         return null;
                     }
                 }, NOPERMS_ACC);
+            } finally {
+                t.setName(name);
+            }
         }
 
         private void run0() {
