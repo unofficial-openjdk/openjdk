a0f026d305cb373342fd2ad602cce644e3f2a026
kshefov <devnull@localhost>
2015-02-11 15:27:03 +0300

    8057008: [TEST_BUG] Test java/awt/Focus/SortingFPT/JDK8048887.java fails with compilation error Reviewed-by: alexsch, serb, kshefov Contributed-by: srinivasan.raghavan@oracle.com

--- test/java/awt/Focus/SortingFPT/JDK8048887.java
+++ test/java/awt/Focus/SortingFPT/JDK8048887.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,13 +22,12 @@
  */
 
 /*
-  @test
-  @bug       8048887
-  @summary   Tests SortingFTP for an exception caused by the tim-sort algo.
-  @author    anton.tarasov: area=awt.focus
-  @run       main JDK8040632
-*/
-
+ @test
+ @bug       8048887
+ @summary   Tests SortingFTP for an exception caused by the tim-sort algo.
+ @author    anton.tarasov: area=awt.focus
+ @run       main JDK8048887
+ */
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
@@ -53,28 +52,31 @@ public class JDK8048887 {
     public void start() {
         final CountDownLatch latch = new CountDownLatch(1);
 
-        SwingUtilities.invokeLater(() -> {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
                 // Catch the original exception which sounds like:
                 // java.lang.IllegalArgumentException: Comparison method violates its general contract!
                 Thread.currentThread().setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-                        public void uncaughtException(Thread t, Throwable e) {
-                            e.printStackTrace();
-                            if (e instanceof IllegalArgumentException) {
-                                passed = false;
-                                latch.countDown();
-                            }
+                    public void uncaughtException(Thread t, Throwable e) {
+                        e.printStackTrace();
+                        if (e instanceof IllegalArgumentException) {
+                            passed = false;
+                            latch.countDown();
                         }
-                    });
+                    }
+                });
 
                 TestDialog d = new TestDialog();
                 // It's expected that the dialog is focused on start.
                 // The listener is called after the FTP completes processing and the bug is reproduced or not.
                 d.addWindowFocusListener(new WindowAdapter() {
-                        public void windowGainedFocus(WindowEvent e) {
-                            latch.countDown();
-                        }
+                    public void windowGainedFocus(WindowEvent e) {
+                        latch.countDown();
+                    }
                 });
                 d.setVisible(true);
+            }
         });
 
         try {
@@ -83,10 +85,11 @@ public class JDK8048887 {
             e.printStackTrace();
         }
 
-        if (passed)
+        if (passed) {
             System.out.println("Test passed.");
-        else
+        } else {
             throw new RuntimeException("Test failed!");
+        }
     }
 }
 
@@ -94,20 +97,19 @@ class TestDialog extends JFrame {
 
     // The layout of the components reproduces the transitivity issue
     // with SortingFocusTraversalPolicy relying on the tim-sort algo.
-
-    private static int[] Xs = new int[] {71, 23, 62, 4, 79, 39, 34, 9, 84, 58, 30, 34, 38, 15, 69, 10, 44, 95, 70, 54,
-    44, 62, 77, 64, 70, 83, 31, 48, 96, 54, 40, 3, 60, 58, 3, 20, 94, 54, 26, 19, 48, 47, 12, 70, 86, 43, 71, 97, 19,
-    69, 90, 22, 43, 76, 10, 60, 29, 49, 9, 9, 15, 73, 85, 80, 81, 35, 87, 43, 17, 57, 38, 44, 29, 86, 96, 15, 57, 26,
-    27, 78, 26, 87, 43, 6, 4, 16, 57, 99, 32, 86, 96, 5, 50, 69, 12, 4, 36, 84, 71, 60, 22, 46, 11, 44, 87, 3, 23, 14,
-    43, 25, 32, 44, 11, 18, 77, 2, 51, 87, 88, 53, 69, 37, 14, 10, 25, 73, 39, 33, 91, 51, 96, 9, 74, 66, 70, 42, 72,
-    7, 82, 40, 91, 33, 83, 54, 33, 50, 83, 1, 81, 32, 66, 11, 75, 56, 53, 45, 1, 69, 46, 31, 79, 58, 12, 20, 92, 49,
-    50, 90, 33, 8, 43, 93, 72, 78, 9, 56, 84, 60, 30, 39, 33, 88, 84, 56, 49, 47, 4, 90, 57, 6, 23, 96, 37, 88, 22, 79,
-    35, 80, 45, 55};
+    private static int[] Xs = new int[]{71, 23, 62, 4, 79, 39, 34, 9, 84, 58, 30, 34, 38, 15, 69, 10, 44, 95, 70, 54,
+        44, 62, 77, 64, 70, 83, 31, 48, 96, 54, 40, 3, 60, 58, 3, 20, 94, 54, 26, 19, 48, 47, 12, 70, 86, 43, 71, 97, 19,
+        69, 90, 22, 43, 76, 10, 60, 29, 49, 9, 9, 15, 73, 85, 80, 81, 35, 87, 43, 17, 57, 38, 44, 29, 86, 96, 15, 57, 26,
+        27, 78, 26, 87, 43, 6, 4, 16, 57, 99, 32, 86, 96, 5, 50, 69, 12, 4, 36, 84, 71, 60, 22, 46, 11, 44, 87, 3, 23, 14,
+        43, 25, 32, 44, 11, 18, 77, 2, 51, 87, 88, 53, 69, 37, 14, 10, 25, 73, 39, 33, 91, 51, 96, 9, 74, 66, 70, 42, 72,
+        7, 82, 40, 91, 33, 83, 54, 33, 50, 83, 1, 81, 32, 66, 11, 75, 56, 53, 45, 1, 69, 46, 31, 79, 58, 12, 20, 92, 49,
+        50, 90, 33, 8, 43, 93, 72, 78, 9, 56, 84, 60, 30, 39, 33, 88, 84, 56, 49, 47, 4, 90, 57, 6, 23, 96, 37, 88, 22, 79,
+        35, 80, 45, 55};
 
     public TestDialog() {
         JPanel panel = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-        for (int i=0; i < Xs.length; i++) {
+        for (int i = 0; i < Xs.length; i++) {
             gbc.gridx = Xs[i];
             gbc.gridy = 100 - gbc.gridx;
             panel.add(new MyComponent(), gbc);
@@ -117,7 +119,8 @@ class TestDialog extends JFrame {
     }
 
     public static class MyComponent extends JPanel {
-        private final static Dimension SIZE = new Dimension(1,1);
+
+        private final static Dimension SIZE = new Dimension(1, 1);
 
         public MyComponent() {
             setBackground(Color.BLACK);
