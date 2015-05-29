c27c08ca9f05a4e75362ee92837d169146708ebe
mchung <devnull@localhost>
2015-05-28 23:30:49 -0700

    Launchers start with -m module-name/main-class as modular application

--- common/autoconf/generated-configure.sh
+++ common/autoconf/generated-configure.sh
@@ -4401,7 +4401,7 @@ VS_SDK_PLATFORM_NAME_2013=
 #CUSTOM_AUTOCONF_INCLUDE
 
 # Do not change or remove the following line, it is needed for consistency checks:
-DATE_WHEN_GENERATED=1431466092
+DATE_WHEN_GENERATED=1432878706
 
 ###############################################################################
 #
