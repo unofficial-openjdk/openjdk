34ff19075085a9524bf5aff371a6fe7e51858a4d
jjg <devnull@localhost>
2015-06-02 18:08:01 -0700

    fix javap test

--- test/tools/javap/classfile/6888367/T6888367.java
+++ test/tools/javap/classfile/6888367/T6888367.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,6 +24,7 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
+
 import com.sun.tools.classfile.*;
 import com.sun.tools.classfile.Type.ArrayType;
 import com.sun.tools.classfile.Type.ClassSigType;
@@ -37,6 +38,7 @@ import com.sun.tools.classfile.Type.WildcardType;
  * @test
  * @bug 6888367
  * @summary classfile library parses signature attributes incorrectly
+ * @modules jdk.compiler/com.sun.tools.classfile
  */
 
 /*
