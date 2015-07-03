0925298b60dd1d2f44efa9250937ad7497ca14ed
xuelei <devnull@localhost>
2015-07-03 16:42:25 +0100

    8067694: Improved certification checking Reviewed-by: mullan, jnimeh, coffeys, robm, asmotrak, ahgross

--- src/share/classes/java/net/URLClassLoader.java
+++ src/share/classes/java/net/URLClassLoader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -752,6 +752,10 @@ public class URLClassLoader extends SecureClassLoader implements Closeable {
             new sun.misc.JavaNetAccess() {
                 public URLClassPath getURLClassPath (URLClassLoader u) {
                     return u.ucp;
+                }
+
+                public String getOriginalHostName(InetAddress ia) {
+                    return ia.holder.getOriginalHostName();
                 }
             }
         );
--- src/share/classes/sun/security/ssl/SSLSocketImpl.java
+++ src/share/classes/sun/security/ssl/SSLSocketImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -42,6 +42,9 @@ import javax.crypto.BadPaddingException;
 import javax.net.ssl.*;
 
 import com.sun.net.ssl.internal.ssl.X509ExtendedTrustManager;
+
+import sun.misc.JavaNetAccess;
+import sun.misc.SharedSecrets;
 
 /**
  * Implementation of an SSL socket.  This is a normal connection type
@@ -387,6 +390,15 @@ final public class SSLSocketImpl extends BaseSSLSocketImpl {
      * will use this to store the data until we actually do the write.
      */
     private ByteArrayOutputStream heldRecordBuffer = null;
+
+    /*
+     * Is the local name service trustworthy?
+     *
+     * If the local name service is not trustworthy, reverse host name
+     * resolution should not be performed for endpoint identification.
+     */
+    static final boolean trustNameService =
+            Debug.getBooleanProperty("jdk.tls.trustNameService", false);
 
     //
     // CONSTRUCTORS AND INITIALIZATION CODE
@@ -2119,10 +2131,40 @@ final public class SSLSocketImpl extends BaseSSLSocketImpl {
     synchronized String getHost() {
         // Note that the host may be null or empty for localhost.
         if (host == null || host.length() == 0) {
-            host = getInetAddress().getHostName();
+            if (!trustNameService) {
+                // If the local name service is not trustworthy, reverse host
+                // name resolution should not be performed for endpoint
+                // identification.  Use the application original specified
+                // hostname or IP address instead.
+                host = getOriginalHostname(getInetAddress());
+            } else {
+                host = getInetAddress().getHostName();
+            }
         }
+
         return host;
     }
+
+    /*
+     * Get the original application specified hostname.
+     */
+    private static String getOriginalHostname(InetAddress inetAddress) {
+        /*
+         * Get the original hostname via sun.misc.SharedSecrets.
+         */
+        JavaNetAccess jna = SharedSecrets.getJavaNetAccess();
+        String originalHostname = jna.getOriginalHostName(inetAddress);
+
+        /*
+         * If no application specified hostname, use the IP address.
+         */
+        if (originalHostname == null || originalHostname.length() == 0) {
+            originalHostname = inetAddress.getHostAddress();
+        }
+
+        return originalHostname;
+    }
+
 
     synchronized String getRawHostname() {
         return rawHostname;
