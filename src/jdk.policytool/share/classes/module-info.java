d7311619b212f8f1cd5541fe93546626e1681574
alanb <devnull@localhost>
2015-07-16 07:46:40 +0100

    Update jdk.policytool's dependences

--- src/jdk.policytool/share/classes/module-info.java
+++ src/jdk.policytool/share/classes/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,5 +25,10 @@
 
 module jdk.policytool {
     requires java.desktop;
+    requires java.logging;
+    requires java.management;
+    requires java.sql;
+    requires java.security.jgss;
+    requires jdk.security.jgss;
 }
 
