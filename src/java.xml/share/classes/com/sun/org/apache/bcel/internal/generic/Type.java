68d098ab8c6a8aafb389cf9ce59d89c4166c53ac
alanb <devnull@localhost>
2015-07-22 14:29:57 +0100

    Prepare for java.lang.ClassFormatException

--- src/java.xml/share/classes/com/sun/org/apache/bcel/internal/generic/Type.java
+++ src/java.xml/share/classes/com/sun/org/apache/bcel/internal/generic/Type.java
@@ -60,6 +60,7 @@ package com.sun.org.apache.bcel.internal.generic;
 
 import com.sun.org.apache.bcel.internal.Constants;
 import com.sun.org.apache.bcel.internal.classfile.*;
+import com.sun.org.apache.bcel.internal.classfile.ClassFormatException;
 import java.util.ArrayList;
 
 /**
