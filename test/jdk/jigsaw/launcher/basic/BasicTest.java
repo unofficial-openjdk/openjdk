caf25eac19fe98f852f87c284e734b408aa03d4a
alanb <devnull@localhost>
2015-07-26 21:16:23 +0100

    Replace remaining launcher shell tests

--- test/jdk/jigsaw/launcher/basic/BasicTest.java
+++ test/jdk/jigsaw/launcher/basic/BasicTest.java
@@ -24,11 +24,13 @@
 /**
  * @test
  * @library ../../lib /lib/testlibrary
- * @build LimitModsTest CompilerUtils jdk.testlibrary.ProcessTools
- * @run testng LimitModsTest
- * @summary Basic tests for java -limitmods
+ * @modules jdk.jlink/jdk.tools.jmod
+ * @build BasicTest CompilerUtils jdk.testlibrary.ProcessTools
+ * @run testng BasicTest
+ * @summary Basic test of starting an application as a module
  */
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -40,7 +42,9 @@ import static org.testng.Assert.*;
 
 
 @Test
-public class LimitModsTest {
+public class BasicTest {
+
+    private static final Path USER_DIR = Paths.get(System.getProperty("user.dir"));
 
     private static final String TEST_SRC = System.getProperty("test.src");
 
@@ -51,7 +55,7 @@ public class LimitModsTest {
     private static final String TEST_MODULE = "test";
 
     // the module main class
-    private static final String MAIN_CLASS = "jdk.test.UseAWT";
+    private static final String MAIN_CLASS = "jdk.test.Main";
 
 
     @BeforeTest
@@ -65,47 +69,65 @@ public class LimitModsTest {
         assertTrue(compiled, "test module did not compile");
     }
 
-    /**
-     * Run class path application with -limitmods
-     */
-    public void testUnnamedModule() throws Exception {
-        String classpath = MODS_DIR.resolve(TEST_MODULE).toString();
-
-        // java -limitmods java.base -cp mods/$TESTMODULE ...
-        int exitValue1
-            = executeTestJava("-limitmods", "java.base",
-                              "-cp", classpath,
-                              MAIN_CLASS)
-                .getExitValue();
-
-        assertTrue(exitValue1 != 0);
-
-
-        // java -limitmods java.base -cp mods/$TESTMODULE ...
-        int exitValue2
-            = executeTestJava("-limitmods", "java.desktop",
-                              "-cp", classpath,
-                             MAIN_CLASS)
-                .getExitValue();
-
-        assertTrue(exitValue2 == 0);
-    }
 
     /**
-     * Run named module with -limitmods
+     * The initial module is loaded from an exploded module
      */
-    public void testNamedModule() throws Exception {
+    public void testRunFromExplodedModule() throws Exception {
         String modulepath = MODS_DIR.toString();
         String mid = TEST_MODULE + "/" + MAIN_CLASS;
 
-        // java -limitmods java.base -mp mods -m $TESTMODULE/$MAINCLASS
+        // java -mp mods -m $TESTMODULE/$MAINCLASS
         int exitValue
-            = executeTestJava("-limitmods", "java.base",
-                              "-mp", modulepath,
+            = executeTestJava("-mp", modulepath,
                               "-m", mid)
                 .getExitValue();
 
         assertTrue(exitValue == 0);
+    }
+
+    /**
+     * The initial module is loaded from a JMOD file
+     */
+    public void testRunFromJMod() throws Exception {
+        Path dir = Files.createTempDirectory(USER_DIR, "mlib");
+
+        // jmod create --class-path mods/${TESTMODULE} \
+        //     --main-class $MAIN_CLASS mlib/${TESTMODULE}.jmod
+        String cp = MODS_DIR.resolve(TEST_MODULE).toString();
+        String jmod = dir.resolve("m.jmod").toString();
+        String[] args = {
+            "create",
+            "--class-path", cp,
+            "--main-class", MAIN_CLASS,
+            jmod
+        };
+        jdk.tools.jmod.JmodTask task = new jdk.tools.jmod.JmodTask();
+        assertEquals(task.run(args), 0);
+
+        // java -mp mods -m $TESTMODULE
+        int exitValue
+            = executeTestJava("-mp", dir.toString(),
+                              "-m", TEST_MODULE)
+                .getExitValue();
+
+        assertTrue(exitValue == 0);
+
+    }
+
+    /**
+     * Attempt to run an unknown module
+     */
+    public void testRunWithUnknownModule() throws Exception {
+        String modulepath = MODS_DIR.toString();
+
+        // java -mp mods -m $TESTMODULE
+        int exitValue
+            = executeTestJava("-mp", modulepath,
+                              "-m", "rhubarb")
+                .getExitValue();
+
+        assertTrue(exitValue != 0);
     }
 
 }
