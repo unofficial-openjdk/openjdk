45dec3e0c5211d9150f23c22dc3749e3cb1a3aa5
mchung <devnull@localhost>
2015-08-14 11:22:34 -0700

    Add test to verify JDK module dependences and loader delegation

--- test/jdk/jigsaw/etc/JdkModules.java
+++ test/jdk/jigsaw/etc/JdkModules.java
@@ -26,18 +26,22 @@
  * @run testng JdkModules
  */
 
-import java.io.*;
-import java.util.*;
-import static java.lang.System.out;
+import java.lang.module.ModuleDescriptor;
+import java.lang.module.ModuleFinder;
+import java.lang.module.ModuleReference;
+import java.lang.reflect.Layer;
+import java.lang.reflect.Module;
+import java.util.Set;
 
-import java.lang.module.*;
 import static java.lang.module.ModuleDescriptor.Requires.Modifier.*;
 
 import org.testng.annotations.*;
+
 import static org.testng.Assert.*;
 
-@Test
 public class JdkModules {
+    private static Set<ModuleReference> mrefs
+            = ModuleFinder.ofInstalled().findAll();
 
     private static ModuleDescriptor base
         = new ModuleDescriptor.Builder("java.base").build();
@@ -56,17 +60,55 @@ public class JdkModules {
         assertTrue(md.requires().containsAll(ref.requires()));
     }
 
-    public void go() {
-        Set<ModuleReference> mrefs
-            = ModuleFinder.ofInstalled().findAll();
-        mrefs.stream().map(ModuleReference::descriptor).forEach(md -> {
-            switch (md.name()) {
-                case "java.base":
-                    check(md, base); break;
-                case "java.compact2":
-                    check(md, compact2); break;
-            }
-        });
+    @Test
+    public void checkJavaBase() {
+        ModuleDescriptor md =
+                mrefs.stream().map(ModuleReference::descriptor)
+                     .filter(d -> d.name().equals("java.base"))
+                     .findFirst().orElseThrow(Error::new);
+
+        check(md, base);
+    }
+    @Test
+    public void checkCompact2() {
+        ModuleDescriptor md =
+                mrefs.stream().map(ModuleReference::descriptor)
+                     .filter(d -> d.name().equals("java.compact2"))
+                     .findFirst().orElseThrow(Error::new);
+        check(md, compact2);
     }
 
+    @Test
+    public void checkLoaderDelegation() {
+        Layer boot = Layer.boot();
+        mrefs.stream().map(ModuleReference::descriptor)
+             .forEach(md -> md.requires().stream().forEach(d ->
+                 {
+                     // check if M requires D and D's loader must be either the
+                     // same or an ancestor of M's loader
+                     ClassLoader loader1 = boot.findLoader(md.name());
+                     ClassLoader loader2 = boot.findLoader(d.name());
+                     if (loader1 != loader2 && !isAncestor(loader2, loader1)) {
+                         // JDK-8130664: jdk.plugin will be refactored
+                         if (!(md.name().equals("jdk.plugin") && d.name().equals("jdk.xml.dom")))
+                             throw new Error(md.name() + " can't delegate to find classes from " + d.name());
+                     }
+                 }));
+    }
+
+    // Returns true if p is an ancestor of cl i.e. class loader 'p' can
+    // be found in the cl's delegation chain
+    private static boolean isAncestor(ClassLoader p, ClassLoader cl) {
+        if (p != null && cl == null) {
+            return false;
+        }
+        ClassLoader acl = cl;
+        do {
+            acl = acl.getParent();
+            if (p == acl) {
+                return true;
+            }
+        } while (acl != null);
+        return false;
+    }
 }
