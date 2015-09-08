ad448e67b2a40782f7d5c060c94b693b9f03c7bb
vinnie <devnull@localhost>
2015-09-08 13:22:18 +0100

    8134014: jdk.naming.dns does not export com.sun.jndi.url.dns to module java.naming Reviewed-by: alanb

--- src/java.corba/share/classes/module-info.java
+++ src/java.corba/share/classes/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -65,6 +65,13 @@ module java.corba {
         jdk.rmic;
     exports com.sun.jndi.cosnaming to
         java.naming;
+    exports com.sun.jndi.url.corbaname to
+        java.naming;
+    exports com.sun.jndi.url.iiop to
+        java.naming;
+    exports com.sun.jndi.url.iiopname to
+        java.naming;
+
     provides javax.naming.spi.InitialContextFactory
         with com.sun.jndi.cosnaming.CNCtxFactory;
 }
