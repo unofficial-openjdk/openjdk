dcb39cd03464df92e28c485ebb65705632786910
vinnie <devnull@localhost>
2015-09-21 14:11:44 +0100

    8136791: module jdk.naming.rmi does not export com.sun.jndi.rmi.registry to module java.rmi Reviewed-by: alanb

--- src/jdk.naming.rmi/share/classes/module-info.java
+++ src/jdk.naming.rmi/share/classes/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -31,5 +31,6 @@ module jdk.naming.rmi {
 
     // temporary export until NamingManager.getURLContext uses services
     exports com.sun.jndi.url.rmi to java.naming;
+    exports com.sun.jndi.rmi.registry to java.rmi;
 }
 
