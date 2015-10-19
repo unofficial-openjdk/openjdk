d9610251b3d663951bb5f65d33e4e598f4cfa0d2
hseigel <devnull@localhost>
2015-10-19 14:22:46 -0400

    Fix for 8081403.  Problem caused by JVM not properly handling module jdk.proxy1's version string of length zero.

--- src/share/vm/classfile/modules.cpp
+++ src/share/vm/classfile/modules.cpp
@@ -81,7 +81,9 @@ static const char* get_module_version(jstring version) {
     module_version = (char *)Modules::default_version();
   } else {
     module_version = java_lang_String::as_utf8_string(JNIHandles::resolve_non_null(version));
-    if (module_version == NULL) module_version = (char *)Modules::default_version();
+    if (module_version == NULL || strlen(module_version) == 0) {
+      module_version = (char *)Modules::default_version();
+    }
   }
   return module_version;
 }
