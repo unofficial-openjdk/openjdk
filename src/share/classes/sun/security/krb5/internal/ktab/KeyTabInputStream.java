dd917f1db86a0db87681d6de4f0d6b3765e93074
weijun <devnull@localhost>
2015-11-04 01:14:23 +0000

    6919610: KeyTabInputStream uses static field for per-instance value Reviewed-by: mullan

--- src/share/classes/sun/security/krb5/internal/ktab/KeyTabInputStream.java
+++ src/share/classes/sun/security/krb5/internal/ktab/KeyTabInputStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2005, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2010, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -48,7 +48,7 @@ import java.io.InputStream;
 public class KeyTabInputStream extends KrbDataInputStream implements KeyTabConstants {
 
     boolean DEBUG = Krb5.DEBUG;
-    static int index;
+    int index;
 
     public KeyTabInputStream(InputStream is) {
         super(is);
