cce5843c223aad215392e12bda72b1a8dc030654
erikj <devnull@localhost>
2015-11-24 11:03:55 +0100

    Fixed images target to properly also build the exploded-image target

--- common/autoconf/generated-configure.sh
+++ common/autoconf/generated-configure.sh
@@ -4653,7 +4653,7 @@ VS_SDK_PLATFORM_NAME_2013=
 #CUSTOM_AUTOCONF_INCLUDE
 
 # Do not change or remove the following line, it is needed for consistency checks:
-DATE_WHEN_GENERATED=1447863188
+DATE_WHEN_GENERATED=1448354029
 
 ###############################################################################
 #
