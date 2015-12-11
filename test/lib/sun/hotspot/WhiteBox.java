2e5337252941934dd0154ffd4fec6144165091ae
mchung <devnull@localhost>
2015-12-10 17:15:59 -0800

    Fixup merge issue

--- test/lib/sun/hotspot/WhiteBox.java
+++ test/lib/sun/hotspot/WhiteBox.java
@@ -419,7 +419,6 @@ public class WhiteBox {
   public native void assertMatchingSafepointCalls(boolean mutexSafepointValue, boolean attemptedNoSafepointValue);
 
   // Sharing
-  public native boolean isSharedClass(Class<?> c);
   public native boolean isShared(Object o);
   public native boolean isSharedClass(Class<?> c);
   public native boolean areSharedStringsIgnored();
