4b126daccd6aa9c96612f2d51df042584d9f1cfa
jlahoda <devnull@localhost>
2016-01-14 21:36:10 +0000

    8135307: CompletionFailure thrown when calling FieldDoc.type, if the field's type is missing Summary: Handling CompletionFailures inside the Javadoc API implementation. Reviewed-by: mcimadamore, ksrini, jjg

--- src/share/classes/com/sun/tools/javadoc/TypeMaker.java
+++ src/share/classes/com/sun/tools/javadoc/TypeMaker.java
@@ -29,6 +29,7 @@ import com.sun.javadoc.*;
 
 import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.code.Symbol.ClassSymbol;
+import com.sun.tools.javac.code.Symbol.CompletionFailure;
 import com.sun.tools.javac.code.Type;
 import com.sun.tools.javac.code.Type.ClassType;
 import com.sun.tools.javac.code.Type.TypeVar;
@@ -44,12 +45,25 @@ public class TypeMaker {
         return getType(env, t, true);
     }
 
+    public static com.sun.javadoc.Type getType(DocEnv env, Type t,
+            boolean errToClassDoc) {
+        try {
+            return getTypeImpl(env, t, errToClassDoc);
+        } catch (CompletionFailure cf) {
+            /* Quietly ignore completion failures and try again - the type
+             * for which the CompletionFailure was thrown shouldn't be completed
+             * again by the completer that threw the CompletionFailure.
+             */
+            return getType(env, t, errToClassDoc);
+        }
+    }
+
     /**
      * @param errToClassDoc  if true, ERROR type results in a ClassDoc;
      *          false preserves legacy behavior
      */
     @SuppressWarnings("fallthrough")
-    public static com.sun.javadoc.Type getType(DocEnv env, Type t,
+    private static com.sun.javadoc.Type getTypeImpl(DocEnv env, Type t,
                                                boolean errToClassDoc) {
         if (env.legacyDoclet) {
             t = env.types.erasure(t);
