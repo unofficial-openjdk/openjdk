52aa87a8091b75f91a328e9609e396300f859990
jjg <devnull@localhost>
2016-01-24 15:43:36 -0800

    Fix typo in comment

--- src/java.compiler/share/classes/javax/lang/model/type/TypeKind.java
+++ src/java.compiler/share/classes/javax/lang/model/type/TypeKind.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -154,7 +154,7 @@ public enum TypeKind {
     INTERSECTION,
 
     /**
-     * A pseudo-type corresponding to a package element.
+     * A pseudo-type corresponding to a module element.
      * @see NoType
      * @since 9
      */
