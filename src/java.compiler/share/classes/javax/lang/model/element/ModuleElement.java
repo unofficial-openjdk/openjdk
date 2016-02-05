28fc69ca55344cba43465ac8eac05ba7212fe8de
jjg <devnull@localhost>
2016-02-05 14:51:46 -0800

    undo bad commit

--- src/java.compiler/share/classes/javax/lang/model/element/ModuleElement.java
+++ src/java.compiler/share/classes/javax/lang/model/element/ModuleElement.java
@@ -50,7 +50,7 @@ public interface ModuleElement extends Element, QualifiedNameable {
      * module, an empty name is returned.
      *
      * @return the simple name of this module or an empty name if
-     * this is an unnamed module
+     * this is an unnamed package
      */
     @Override
     Name getSimpleName();
