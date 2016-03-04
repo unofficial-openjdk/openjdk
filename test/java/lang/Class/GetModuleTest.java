0a9854973c622553a1bd0c07fc019c9097a2d655
shurailine <devnull@localhost>
2016-03-03 16:05:08 -0800

    Minor clean up on test module dependences

--- test/java/lang/Class/GetModuleTest.java
+++ test/java/lang/Class/GetModuleTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,7 +25,7 @@
  * @test
  * @summary Exercise Class#getModule
  * @modules java.base/jdk.internal.org.objectweb.asm
-            java.desktop
+ *          java.desktop
  * @run testng GetModuleTest
  */
 
@@ -83,7 +83,7 @@ public class GetModuleTest {
         };
     }
 
-    @Test(dataProvider = "testClasses")
+    @Test(dataProvider = "testclasses")
     public void testGetModule(Class<?> type, String expected) {
         Module m = type.getModule();
         assertNotNull(m);
