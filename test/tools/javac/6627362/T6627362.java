d85fccb805a21bc1a492109238ef2d431bf166ae
jjg <devnull@localhost>
2016-03-08 15:45:13 -0800

    unexport javap

--- test/tools/javac/6627362/T6627362.java
+++ test/tools/javac/6627362/T6627362.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -27,6 +27,7 @@
  * @summary javac generates code that uses array.clone,
  *          which is not available on JavaCard
  * @modules jdk.compiler
+ *          jdk.jdeps/com.sun.tools.javap
  */
 
 import java.io.*;
