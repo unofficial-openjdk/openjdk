f8ec9a729cfa0edeb3616da22652d2b12c083a2f
mchung <devnull@localhost>
2016-03-10 14:25:05 -0800

    launcher review comment by ksrini

--- src/java.base/share/classes/sun/launcher/LauncherHelper.java
+++ src/java.base/share/classes/sun/launcher/LauncherHelper.java
@@ -493,12 +493,8 @@ public enum LauncherHelper {
                                             String what) {
         initOutput(printToStderr);
 
-        Class<?> mainClass;
-        if (mode == LM_MODULE) {
-            mainClass = loadModuleMainClass(what);
-        } else {
-            mainClass = loadMainClass(mode, what);
-        }
+        Class<?> mainClass = (mode == LM_MODULE) ? loadModuleMainClass(what)
+                                                 : loadMainClass(mode, what);
 
         validateMainClass(mainClass);
 
@@ -866,10 +862,6 @@ public enum LauncherHelper {
                     || fxLaunchName == null) {
                 throw new RuntimeException("Invalid JavaFX launch parameters");
             }
-            AccessController.doPrivileged((PrivilegedAction<Void>) () -> {
-                fxLauncherMethod.setAccessible(true);
-                return null;
-            });
             // launch appClass via fxLauncherMethod
             fxLauncherMethod.invoke(null,
                     new Object[] {fxLaunchName, fxLaunchMode, args});
@@ -957,7 +949,6 @@ public enum LauncherHelper {
                     for (String impl : ps.providers())
                         ostream.format("  provides %s with %s%n", ps.service(), impl);
                 }
-
             }
         }
     }
@@ -969,5 +960,4 @@ public enum LauncherHelper {
         else
             return md.toNameAndVersion() + " (" + loc + ")";
     }
-
 }
