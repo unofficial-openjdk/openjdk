da1d19b9f3bf5377a0539249c2a884b41537f6c2
alanb <devnull@localhost>
2016-03-14 12:09:37 +0000

    Review comments

--- src/java.base/share/classes/java/lang/module/ModuleReader.java
+++ src/java.base/share/classes/java/lang/module/ModuleReader.java
@@ -30,7 +30,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.nio.ByteBuffer;
-import java.util.Arrays;
 import java.util.Optional;
 
 
@@ -143,46 +142,12 @@ public interface ModuleReader extends Closeable {
      * @see ClassLoader#defineClass(String, ByteBuffer, java.security.ProtectionDomain)
      */
     default Optional<ByteBuffer> read(String name) throws IOException {
-        final int BUFFER_SIZE = 8192;
-        final int MAX_BUFFER_SIZE = Integer.MAX_VALUE - 8;
-
-        InputStream in = open(name).orElse(null);
-        if (in == null) {
-            // not found
+        Optional<InputStream> in = open(name);
+        if (in.isPresent()) {
+            byte[] bytes = in.get().readAllBytes();
+            return Optional.of(ByteBuffer.wrap(bytes));
+        } else {
             return Optional.empty();
-        }
-
-        try (in) {
-            int capacity = in.available();
-            if (capacity == 0)
-                capacity = BUFFER_SIZE;
-
-            byte[] buf = new byte[capacity];
-            int nread = 0;
-            int n;
-            for (;;) {
-                // read to EOF
-                while ((n = in.read(buf, nread, capacity - nread)) > 0)
-                    nread += n;
-
-                // if last call to source.read() returned -1, we are done
-                // otherwise, try to read one more byte; if that failed we're done too
-                if (n < 0 || (n = in.read()) < 0)
-                    break;
-
-                // one more byte was read; need to allocate a larger buffer
-                if (capacity <= MAX_BUFFER_SIZE - capacity) {
-                    capacity = Math.max(capacity << 1, BUFFER_SIZE);
-                } else {
-                    if (capacity == MAX_BUFFER_SIZE)
-                        throw new OutOfMemoryError("Required array size too large");
-                    capacity = MAX_BUFFER_SIZE;
-                }
-                buf = Arrays.copyOf(buf, capacity);
-                buf[nread++] = (byte) n;
-            }
-
-            return Optional.of(ByteBuffer.wrap(buf, 0, nread));
         }
     }
 
