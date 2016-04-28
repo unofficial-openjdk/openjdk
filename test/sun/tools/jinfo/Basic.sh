55839a0fb262fdfc41fe394fd39478996831c191
weijun <devnull@localhost>
2016-04-28 13:14:31 +0100

    7104161: test/sun/tools/jinfo/Basic.sh fails on Ubuntu Reviewed-by: alanb

--- test/sun/tools/jinfo/Basic.sh
+++ test/sun/tools/jinfo/Basic.sh
@@ -50,7 +50,17 @@ if [ $isMacos = true -o $isAIX = true -o `uname -m` = ppc64 ]; then
     runSA=false
 fi
 
-if [ $isWindows = false ]; then
+if [ $isLinux = true ]; then
+    # Some Linux systems disable non-child ptrace (see 7050524)
+    ptrace_scope=`/sbin/sysctl -n kernel.yama.ptrace_scope`
+    if [ $? = 0 ]; then
+        if [ $ptrace_scope = 1 ]; then
+            runSA=false
+        fi
+    fi
+fi
+
+if [ $runSA = true ]; then
     # -sysprops option
     ${JINFO} -sysprops $appJavaPid
     if [ $? != 0 ]; then failed=1; fi
