c61fb0f49544adb7aa62c3f972a01104afd67939
alanb <devnull@localhost>
2016-06-19 09:37:41 +0100

    Add @Deprecated to java.corba module

--- src/java.corba/share/classes/module-info.java
+++ src/java.corba/share/classes/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,6 +23,7 @@
  * questions.
  */
 
+@Deprecated(since="9", forRemoval=true)
 module java.corba {
     requires public java.desktop;
     requires public java.rmi;
