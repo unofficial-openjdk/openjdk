c0493314eab789d341c55068ee25ebc15b30616c
weijun <devnull@localhost>
2016-08-09 01:17:58 +0100

    8027026: Change keytool -genkeypair to use -keyalg RSA Reviewed-by: alanb, chegar, mullan

--- test/sun/security/pkcs12/PKCS12SameKeyId.java
+++ test/sun/security/pkcs12/PKCS12SameKeyId.java
@@ -59,7 +59,7 @@ public class PKCS12SameKeyId {
         for (int i=0; i<SIZE; i++) {
             System.err.print(".");
             String cmd = "-keystore " + JKSFILE
-                    + " -storepass changeit -keypass changeit "
+                    + " -storepass changeit -keypass changeit -keyalg rsa "
                     + "-genkeypair -alias p" + i + " -dname CN=" + i;
             KeyTool.main(cmd.split(" "));
         }
