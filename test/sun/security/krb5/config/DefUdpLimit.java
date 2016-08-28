966d61c31c370c63be1a3e8ab2f3db04ac873a4f
alanb <devnull@localhost>
2016-08-28 07:28:19 +0100

    Update further tests using setAccessible

--- test/sun/security/krb5/config/DefUdpLimit.java
+++ test/sun/security/krb5/config/DefUdpLimit.java
@@ -25,7 +25,7 @@
  * @test
  * @bug 8009875
  * @summary Provide a default udp_preference_limit for krb5.conf
- * @modules java.security.jgss/sun.security.krb5
+ * @modules java.security.jgss/sun.security.krb5:private
  * @compile -XDignore.symbol.file DefUdpLimit.java
  * @run main/othervm DefUdpLimit -1 1465
  * @run main/othervm DefUdpLimit 0 0
