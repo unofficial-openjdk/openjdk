14ad4fe8c4d1cd8347d1242577ef15dfe03be577
mchung <devnull@localhost>
2016-11-09 09:57:18 -0800

    open classes passed to JAXB context for deep reflection

--- src/java.xml.ws/share/classes/module-info.java
+++ src/java.xml.ws/share/classes/module-info.java
@@ -110,6 +110,9 @@ module java.xml.ws {
     // com.sun.xml.internal.ws.fault.SOAPFaultBuilder uses JAXBContext.newInstance
     opens com.sun.xml.internal.ws.fault to java.xml.bind;
 
+    // classes passed to JAXBContext.newInstance for deep reflection
+    opens com.sun.xml.internal.ws.addressing to java.xml.bind;
+
     // JAF data handlers
     exports com.sun.xml.internal.messaging.saaj.soap to
         java.activation;
