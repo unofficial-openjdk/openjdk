5f531ab7abc01c4d496a62d962125a768e6aa403
alanb <devnull@localhost>
2017-02-06 16:15:49 +0000

    More startup improvements Contributed-by: claes.redestad@oracle.com

--- src/java.base/share/classes/jdk/internal/module/ModuleBootstrap.java
+++ src/java.base/share/classes/jdk/internal/module/ModuleBootstrap.java
@@ -118,7 +118,12 @@ public final class ModuleBootstrap {
         long t0 = System.nanoTime();
 
         // system modules (may be patched)
-        ModuleFinder systemModules = ModuleFinder.ofSystem();
+        ModuleFinder systemModules;
+        if (SystemModules.MODULE_NAMES.length > 0) {
+            systemModules = SystemModuleFinder.getInstance();
+        } else {
+            systemModules = ModuleFinder.ofSystem();
+        }
 
         PerfCounters.systemModulesTime.addElapsedTimeFrom(t0);
 
