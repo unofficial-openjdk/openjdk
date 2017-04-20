58ecff491d4ccf5799e3b9e87a0b99423d9a6560
alanb <devnull@localhost>
2017-04-20 16:56:33 +0100

    Dynamic loading of JMX or tool agent into modular application

--- src/share/vm/classfile/modules.cpp
+++ src/share/vm/classfile/modules.cpp
@@ -454,7 +454,6 @@ void Modules::define_module(jobject module, jstring version,
   // If the module is defined to the boot loader and an exploded build is being
   // used, prepend <java.home>/modules/modules_name, if it exists, to the system boot class path.
   if (loader == NULL &&
-      !Universe::is_module_initialized() &&
       !ClassLoader::has_jrt_entry()) {
     ClassLoader::add_to_exploded_build_list(module_symbol, CHECK);
   }
