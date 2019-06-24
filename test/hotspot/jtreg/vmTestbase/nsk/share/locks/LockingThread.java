174e913a3d0e5f98c298e54e76186210a8748f20
cjplummer <devnull@localhost>
2019-06-24 14:57:34 -0700

    Adust owned monitor tests for extra Thread.sleep0() frame.

--- test/hotspot/jtreg/vmTestbase/nsk/share/locks/LockingThread.java
+++ test/hotspot/jtreg/vmTestbase/nsk/share/locks/LockingThread.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -487,8 +487,8 @@ public class LockingThread extends Thread {
     // calculate stack depth at which monitor was acquired
     int expectedDepth() {
         // for each monitor call 2 methods: createStackFrame() and method which acquire monitor
-        // + when stack creation is finished call 3 methods: createStackFrame()->doWait()->sleep()
-        return (stackFramesDescription.size() - currentIndex) * 2 + 3;
+        // + when stack creation is finished call 4 methods: createStackFrame()->doWait()->sleep()->sleep0()
+        return (stackFramesDescription.size() - currentIndex) * 2 + 4;
     }
 
     private native void nativeJNIMonitorEnter(Object object);
